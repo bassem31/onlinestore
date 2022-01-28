941d86e6a77aace1d0ea9d4bf2e31252826f3e35
bassem31 <souissi.bassem87@gmail.com>
2022-01-28 09:50:50 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './shared/aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/aboutus',pathMatch:'full'},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'**',redirectTo:'/aboutus',pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { CustomaterialModule } from './customaterial/customaterial.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
